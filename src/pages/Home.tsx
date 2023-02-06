c8ca3264e3ead12e47a2c086d782f25fbf489cb4
hitalo <hitalo.2014.hs@gmail.com>
2023-02-06 10:21:21 -0300

    feat: todo-basic functionalities

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((oldState) => [...oldState, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const taskListChecked = tasks.map((task) => ({ ...task }));
+
+    const taskIndex = taskListChecked.find((item) => item.id === id);
+
+    if (taskIndex) taskIndex.done = !taskIndex.done;
+
+    setTasks(taskListChecked);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const taskListUpdated = tasks.filter((item) => item.id !== id);
+    setTasks(taskListUpdated);
   }
 
   return (
@@ -26,18 +39,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
